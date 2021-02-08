6c6a6f411d1a26e8066c5dc06738491b9a63e16b
collinradichel <radichel.collin@gmail.com>
2021-02-08 14:13:05 -0600

    added buttons to dashboard (very roughly) what it's going to look like.  Just three buttons.  Going to work on my AddDisc Component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import React from "react";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
 
 function Dashboard() {
-  // this component doesn't do much to start, just renders some user reducer info to the DOM
+  const classes = useStyles();
   const user = useSelector((store) => store.user);
+
   return (
-    <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid>
+          <Button variant="contained" color="primary">
+            Inventory
+          </Button>
+        </Grid>
+        <Grid>
+          <Button
+            variant="contained"
+            color="secondary"
+            onClick={() => {
+              history.push("/addDisc");
+            }}
+          >
+            Add Disc
+          </Button>
+        </Grid>
+        <Grid>
+          <Button variant="outlined" color="secondary">
+            Give Suggestion
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
